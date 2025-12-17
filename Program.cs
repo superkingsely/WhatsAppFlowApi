906e7bb4ddcaa3cea99808f3c627b9dabe9ce2d8
superkingsely <superkingsely@gmail.com>
2025-12-17 21:51:07 +0100

    cool

---
+++ Program.cs
@@ -0,0 +1,87 @@
+using Microsoft.AspNetCore.Http.Json;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Needed for Flow JSON size
+builder.Services.Configure<JsonOptions>(options =>
+{
+    options.SerializerOptions.PropertyNamingPolicy = null;
+});
+
+var app = builder.Build();
+
+/**
+ * HEALTH CHECK
+ * Meta calls this first
+ */
+app.MapGet("/", () =>
+{
+    return Results.Ok(new { status = "ok" });
+});
+
+/**
+ * FLOW ENDPOINT
+ * Meta sends submitted flow data here
+ */
+app.MapPost("/flow", async (HttpRequest request) =>
+{
+    using var reader = new StreamReader(request.Body);
+    var body = await reader.ReadToEndAsync();
+
+    Console.WriteLine("FLOW DATA RECEIVED:");
+    Console.WriteLine(body);
+
+    // REQUIRED RESPONSE
+    return Results.Ok(new
+    {
+        status = "success"
+    });
+});
+
+app.Run();
+
+
+
+
+
+// var builder = WebApplication.CreateBuilder(args);
+
+// // Add services to the container.
+// // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+// builder.Services.AddOpenApi();
+
+// var app = builder.Build();
+
+// // Configure the HTTP request pipeline.
+// if (app.Environment.IsDevelopment())
+// {
+//     app.MapOpenApi();
+// }
+
+// app.UseHttpsRedirection();
+
+// var summaries = new[]
+// {
+//     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+// };
+
+// app.MapGet("/weatherforecast", () =>
+// {
+//     var forecast =  Enumerable.Range(1, 5).Select(index =>
+//         new WeatherForecast
+//         (
+//             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+//             Random.Shared.Next(-20, 55),
+//             summaries[Random.Shared.Next(summaries.Length)]
+//         ))
+//         .ToArray();
+//     return forecast;
+// })
+// .WithName("GetWeatherForecast");
+
+// app.Run();
+
+// record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+// {
+//     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+// }
